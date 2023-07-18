d946d03e0f89d53626e4c945b484656d5c62bf07
lagzian <134182864+lagzian@users.noreply.github.com>
2023-07-18 13:04:50 +0330

    Update gmailbot.py

--- gmailbot.py
+++ gmailbot.py
@@ -10,73 +10,73 @@ from email.header import decode_header
 
 async def fetch_emails_and_send_telegram():
 
-  # Get Telegram bot token
-  bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    # Get Telegram bot token
+    bot_token = os.environ['TELEGRAM_BOT_TOKEN']
   
-  # Initialize Telegram bot
-  bot = Bot(token=bot_token)
+    # Initialize Telegram bot
+    bot = Bot(token=bot_token)
 
-  # Get Telegram chat ID
-  chat_id = os.environ['TELEGRAM_CHAT_ID']
+    # Get Telegram chat ID
+    chat_id = os.environ['TELEGRAM_CHAT_ID']
 
-  # Gmail credentials
-  username = 'your_gmail@gmail.com'
-  password = os.environ['GMAIL_APP_PASSWORD']
+    # Gmail credentials
+    username = 'your_gmail@gmail.com'
+    password = os.environ['GMAIL_APP_PASSWORD']
 
-  # Connect to Gmail
-  mail = imaplib.IMAP4_SSL('imap.gmail.com')
-  mail.login(username, password)
+    # Connect to Gmail
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login(username, password)
 
-  # Select inbox
-  mail.select('INBOX')
+    # Select inbox
+    mail.select('INBOX')
 
-  _, search_data = mail.search(None, 'UNSEEN')
-  email_ids = search_data[0].split()
+    _, search_data = mail.search(None, 'UNSEEN')
+    email_ids = search_data[0].split()
 
-  for email_id in email_ids:
-    _, msg_data = mail.fetch(email_id, '(RFC822)')
-    raw_email = msg_data[0][1]
-    
-    # Parse email
-    email_msg = email.message_from_bytes(raw_email)
+    for email_id in email_ids:
+        _, msg_data = mail.fetch(email_id, '(RFC822)')
+        raw_email = msg_data[0][1]
+        
+        # Parse email
+        email_msg = email.message_from_bytes(raw_email)
 
-    # Process email fields
-    subject = decode_header(email_msg['Subject'])[0][0]
-    sender = decode_header(email_msg['From'])[0][0]
-    body = get_email_body(email_msg)
-    
-    # Build Telegram message
-    message = build_telegram_message(subject, sender, body, email_id)
-    
-    # Send message with delete button
-    sent_msg = await bot.send_message(chat_id=chat_id, text=message, reply_markup=build_keyboard(email_id))
+        # Process email fields
+        subject = decode_header(email_msg['Subject'])[0][0]
+        sender = decode_header(email_msg['From'])[0][0]
+        body = get_email_body(email_msg)
+        
+        # Build Telegram message
+        message = build_telegram_message(subject, sender, body, email_id)
+        
+        # Send message with delete button
+        sent_msg = await bot.send_message(chat_id=chat_id, text=message, reply_markup=build_keyboard(email_id))
 
-  mail.logout()
+    mail.logout()
 
 def get_email_body(email_msg):
-  # Logic to extract plain text body
+    # Logic to extract plain text body
 
 def build_telegram_message(subject, sender, body, email_id):
-  # Logic to build message string
-  
+    # Logic to build message string
+
 def build_keyboard(email_id):
-  keyboard = [[InlineKeyboardButton("Delete", callback_data=email_id)]]
-  return InlineKeyboardMarkup(keyboard)
+    keyboard = [[InlineKeyboardButton("Delete", callback_data=email_id)]]
+    return InlineKeyboardMarkup(keyboard)
 
 async def delete_email(update, context):
-  query = update.callback_query
-  email_id = query.data
-  
-  # Delete email on server
-  mail.store(email_id , '+FLAGS', '\\Deleted') 
-  
-  await query.answer()
-  await query.edit_message_reply_markup(reply_markup=None)
+    query = update.callback_query
+    email_id = query.data
+    
+    # Delete email on server
+    mail.store(email_id , '+FLAGS', '\\Deleted') 
+    
+    await query.answer()
+    await query.edit_message_reply_markup(reply_markup=None)
 
 if __name__ == '__main__':
 
-  # Add callback handler
-  dp.add_handler(CallbackQueryHandler(delete_email))
+    # Add callback handler
+    dp.add_handler(CallbackQueryHandler(delete_email))
 
-  # Run main function
-  asyncio.run(fetch_emails_and_send_telegram())
+    # Run main function
+    asyncio.run(fetch_emails_and_send_telegram())
