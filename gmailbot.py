4f8a3be31c3e0c54532cc17e9371775b3fefa035
lagzian <134182864+lagzian@users.noreply.github.com>
2023-07-18 12:12:52 +0330

    Update gmailbot.py

--- gmailbot.py
+++ gmailbot.py
@@ -4,6 +4,7 @@ import email
 import os
 from telegram import Bot
 from bs4 import BeautifulSoup
+from email.header import decode_header
 
 async def fetch_emails_and_send_telegram():
     # Get the Telegram bot token from GitHub secret
@@ -43,9 +44,16 @@ async def fetch_emails_and_send_telegram():
         raw_email = msg_data[0][1]
         email_message = email.message_from_bytes(raw_email)
 
+        # Decode subject and sender headers
+        subject = decode_header(email_message['Subject'])[0][0]
+        if isinstance(subject, bytes):
+            subject = subject.decode('utf-8')
+        
+        sender = decode_header(email_message['From'])[0][0]
+        if isinstance(sender, bytes):
+            sender = sender.decode('utf-8')
+
         # Extract the desired information from the email
-        subject = email_message['Subject']
-        sender = email_message['From']
         body = ''
 
         if email_message.is_multipart():
