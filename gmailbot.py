71da6f11c2526ff28724e02970e1df4e81fb86c8
lagzian <134182864+lagzian@users.noreply.github.com>
2023-08-14 11:18:13 +0330

    Update gmailbot.py

--- gmailbot.py
+++ gmailbot.py
@@ -3,7 +3,8 @@ import imaplib
 import email
 import os
 
-from telegram import Bot, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Bot
+from telegram.ext import Dispatcher, CommandHandler  
 from bs4 import BeautifulSoup
 from email.header import decode_header
 
@@ -69,7 +70,7 @@ async def fetch_emails_and_send_telegram(delete_emails=False):
           break
 
     # Create Telegram message
-    header = "🔔📧📭NEW EMAIL📭📧🔔"
+    header = "🔔📧📭NEW EMAIL📭📧🔔"  
     msg = f"{header}\nSubject: {subject}\nFrom: {sender}\n\n{body}"
     if len(msg) > 4096:
       msg = msg[:4093] + "..."
@@ -79,7 +80,6 @@ async def fetch_emails_and_send_telegram(delete_emails=False):
     reply_markup = ReplyKeyboardMarkup([[button]], one_time_keyboard=True)
     await bot.send_message(chat_id, text=msg, reply_markup=reply_markup)
 
-  # Only delete emails if specified
   if delete_emails:
     mail.store(email_id, '+FLAGS', '(\\Deleted)')
 
@@ -88,12 +88,15 @@ def delete_handler(update, context):
   asyncio.run(fetch_emails_and_send_telegram(delete_emails))
 
 def main():
+
   bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+
   bot = Bot(token=bot_token)
 
-  bot.message_handler(delete_handler)
+  dispatcher = Dispatcher(bot, None, workers=0)
+  dispatcher.add_handler(CommandHandler('delete', delete_handler))
 
-  asyncio.run(fetch_emails_and_send_telegram())
+  asyncio.run(fetch_emails_and_send_telegram())  
 
 if __name__ == '__main__':
   main()
