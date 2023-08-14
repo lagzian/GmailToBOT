ed0130ad08a1cd47cfea12377efb0ba31c942858
lagzian <134182864+lagzian@users.noreply.github.com>
2023-08-14 10:39:06 +0330

    Update gmailbot.py

--- gmailbot.py
+++ gmailbot.py
@@ -2,13 +2,11 @@ import asyncio
 import imaplib
 import email
 import os
-from telegram import Bot, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Bot
 from bs4 import BeautifulSoup
 from email.header import decode_header
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
 
 async def fetch_emails_and_send_telegram():
-
     # Get the Telegram bot token from GitHub secret
     bot_token = os.environ['TELEGRAM_BOT_TOKEN']
 
@@ -52,16 +50,18 @@ async def fetch_emails_and_send_telegram():
         subject = decode_header(email_message['Subject'])[0][0]
         if isinstance(subject, bytes):
             subject = subject.decode('utf-8')
-
+        
         sender = decode_header(email_message['From'])[0][0]
         if isinstance(sender, bytes):
             sender = sender.decode('utf-8')
 
-        # Extract the desired information from the email body
+        # Extract the desired information from the email
         body = ''
+
         if email_message.is_multipart():
             for part in email_message.get_payload():
                 content_type = part.get_content_type()
+
                 if content_type == 'text/plain':
                     charset = part.get_content_charset()
                     body = part.get_payload(decode=True)
@@ -70,6 +70,7 @@ async def fetch_emails_and_send_telegram():
                     else:
                         body = body.decode('utf-8', 'ignore')
                     break
+
                 elif content_type == 'text/html':
                     body = part.get_payload(decode=True)
                     soup = BeautifulSoup(body, 'lxml')
@@ -77,22 +78,18 @@ async def fetch_emails_and_send_telegram():
                     break
 
         # Add "📧NEW EMAIL📧" header to the message
-        header = "🔔📧📭NEW EMAIL📭📧🔔"  
+        header = "🔔📧📭NEW EMAIL📭📧🔔"
         message = f"{header}\nSubject: {subject}\nFrom: {sender}\n\n{body}"
 
         # Truncate the message if it exceeds the limit
         if len(message) > 4096:
             message = message[:4093] + "..."
 
-        # Send the message to Telegram with a delete button
-        button = KeyboardButton('Delete')
-      
-        # Pass the keyboard in ReplyKeyboardMarkup 
-        reply_markup = ReplyKeyboardMarkup(keyboard=[[button]], one_time_keyboard=True)
-        await bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
+        # Send the message to Telegram
+        await bot.send_message(chat_id=chat_id, text=message)
 
-        # Delete the email from Gmail
-        mail.store(email_id, '+FLAGS', '(\\Deleted)')
+    # Logout and close the connection
+    mail.logout()
 
-if __name__ == '__main__':
-    asyncio.run(fetch_emails_and_send_telegram())
+# Run the function in an asynchronous event loop
+asyncio.run(fetch_emails_and_send_telegram())
